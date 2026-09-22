b93fe51d779d52b2042a3083dd6c970f49e25fff
Kojo6771 <230165858@aston.ac.uk>
2026-09-23 00:17:33 +0100

    Imported libraries
    I have imported the necessary libraries and saved into the requirements.txt

---
+++ main.py
@@ -0,0 +1,14 @@
+#Library imports
+import warnings
+warnings.filterwarnings("ignore")
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+# NLTK setup for text processing
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
